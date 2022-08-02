f7c18be2b2b405f3a117046c03cf5d26ed19c4b8
ilyass.elfouih <ilyass.elfouih@gmail.com>
2022-08-02 19:37:27 +0100

    feat: add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'leaderboard-of-nonsense-1dd27',
+    appId: '1:896982615408:web:a1654a44107fd804d97bb5',
+    storageBucket: 'leaderboard-of-nonsense-1dd27.appspot.com',
+    apiKey: 'AIzaSyDzFelJXlKeVyi535ih6GisWTlP8anpJII',
+    authDomain: 'leaderboard-of-nonsense-1dd27.firebaseapp.com',
+    messagingSenderId: '896982615408',
+    measurementId: 'G-KE82VZGT0G',
+  },
   production: true
 };
