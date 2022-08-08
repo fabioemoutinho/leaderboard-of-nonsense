429d2a7952372eef69480b99171609878014829d
fabioemoutinho <fabioemoutinho@gmail.com>
2022-08-08 12:32:11 -0300

    fix: windows 95 UI kit style reference, remove unused variable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from "@angular/common/http";
 
 const routes: Routes = [
   {
-    path:'',
-    pathMatch:'full',
-    redirectTo:'leader-board'
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'leader-board',
   },
   {
     path: 'leader-board',
-    loadChildren: () => import('./leaderboard/leaderboard.module').then(m => m.LeaderboardModule)
-  }
+    loadChildren: () =>
+      import('./leaderboard/leaderboard.module').then(
+        (m) => m.LeaderboardModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
