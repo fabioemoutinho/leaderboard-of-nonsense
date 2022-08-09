73f9edc76f8e98ea403cc8bf1706635d1838fa28
ilyass.elfouih <ilyass.elfouih@gmail.com>
2022-08-09 21:08:48 +0100

    feat: :sparkles: show login or avatar if logged in

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Auth, GoogleAuthProvider, signInWithPopup, UserCredential } from '@angular/fire/auth';
-import { BehaviorSubject, from } from 'rxjs';
+import { Auth, GoogleAuthProvider, signInWithPopup, authState, browserSessionPersistence, User } from '@angular/fire/auth';
+import { BehaviorSubject, from, mergeMap, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  readonly user$ = new BehaviorSubject<UserCredential | null>(null);
+  readonly user$ = new BehaviorSubject<User | null>(null);
 
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth) {
+    from(this.auth.setPersistence(browserSessionPersistence)).pipe(
+      mergeMap(() => authState(this.auth)),
+      tap((user) => {
+        if (user) {
+          this.user$.next(user);
+        }
+      }),
+    ).subscribe();
+  }
 
-  onLogin(){
+  onLogin() {
     const googleProvider = new GoogleAuthProvider();
-   from(signInWithPopup(this.auth,googleProvider))
-   .subscribe((value) => {
-    this.user$.next(value)
-  });
+    return from(signInWithPopup(this.auth, googleProvider)).subscribe(user => this.user$.next(user.user));
   }
 }
