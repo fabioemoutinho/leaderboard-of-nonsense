3daefe199edfb088c64a2418d397d83b2834855b
ilyass.elfouih <ilyass.elfouih@gmail.com>
2022-08-16 19:08:31 +0100

    chore(login): :sparkles: fix the login with firebase

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Auth, GoogleAuthProvider, signInWithPopup, authState, browserSessionPersistence, User } from '@angular/fire/auth';
-import { BehaviorSubject, from, mergeMap, tap } from 'rxjs';
+import { Auth, GoogleAuthProvider, signInWithPopup, authState, browserSessionPersistence } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  readonly user$ = new BehaviorSubject<User | null>(null);
+  readonly user$ = authState(this.auth);
 
   constructor(private auth: Auth) {
-    from(this.auth.setPersistence(browserSessionPersistence)).pipe(
-      mergeMap(() => authState(this.auth)),
-      tap((user) => {
-        if (user) {
-          this.user$.next(user);
-        }
-      }),
-    ).subscribe();
+    this.auth.setPersistence(browserSessionPersistence);
   }
 
-  onLogin() {
+  onLogin(): void {
     const googleProvider = new GoogleAuthProvider();
-    return from(signInWithPopup(this.auth, googleProvider)).subscribe(user => this.user$.next(user.user));
+    signInWithPopup(this.auth, googleProvider);
   }
 }
